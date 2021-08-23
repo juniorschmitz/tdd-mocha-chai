51511f54f75f6e69a67d43c79316271ed2d77bb8
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-08-23 18:42:37 -0300

    add: new tests for other operations

--- tests/calculator-tests.js
+++ tests/calculator-tests.js
@@ -11,5 +11,17 @@ describe('The Calculator', () => {
     it('Should sum two positive integers', () => {
       expect(calculator.sum(2, 3)).to.equal(5);
     });
+
+    it('Should sub two positive integers', () => {
+      expect(calculator.sub(3, 2)).to.equal(1);
+    });
+
+    it('Should mul two positive integers', () => {
+      expect(calculator.mul(5, 5)).to.equal(25);
+    });
+
+    it('Should div two positive integers', () => {
+      expect(calculator.div(10, 2)).to.equal(5);
+    });
   
 });
