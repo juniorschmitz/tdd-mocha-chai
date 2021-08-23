d4fbb36674a612286fce16bcbdb32ba858ae59c6
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-08-23 18:42:47 -0300

    add: implementation for the new tests

--- src/calculator.js
+++ src/calculator.js
@@ -2,4 +2,16 @@ module.exports = class Calculator {
   sum(a, b) {
     return a + b
   }
+
+  sub(a, b) {
+    return a - b
+  }
+
+  mul(a, b) {
+    return a * b
+  }
+
+  div(a, b) {
+    return a / b
+  }
 }
