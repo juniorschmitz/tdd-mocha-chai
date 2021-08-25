6016d5b5ebdb77119321509979f27f006dd127af
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-08-25 17:47:46 -0300

    add: new scenarios for the sum feature

--- tests/calculator-tests.js
+++ tests/calculator-tests.js
@@ -12,8 +12,28 @@ describe('The Calculator', () => {
       expect(calculator.sum(2, 3)).to.equal(5);
     });
 
-    it('Should sum many numbers', () => {
+    it('Should sum a positive integer and zero', () => {
+      expect(calculator.sum(2, 0)).to.equal(2);
+    });
+
+    it('Should sum a negative integer and zero', () => {
+      expect(calculator.sum(-5, 0)).to.equal(-5);
+    });
+
+    it('Should sum a positive and a negative integers', () => {
+      expect(calculator.sum(-20, 10)).to.equal(-10);
+    });
+
+    it('Should sum two negative integers', () => {
+      expect(calculator.sum(-2, -3)).to.equal(-5);
+    });
+
+    it('Should sum many positive numbers', () => {
       expect(calculator.sum(2, 3, 7, 12)).to.equal(24);
+    });
+
+    it('Should sum many positive and negative numbers', () => {
+      expect(calculator.sum(2, 3, 7, -12, -20, 20, 10)).to.equal(10);
     });
 
     it('Should sub two positive integers', () => {
